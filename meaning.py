544af7614eeaa516b58b6a0fc74a13f329caf25d
antonomics <antonomics101@gmail.com>
2024-12-10 09:35:42 +0300

    hg

--- meaning.py
+++ meaning.py
@@ -1,6 +1,10 @@
-meaning = 42
+meaning = 4
 print('')
-if meaning > 10:
-    print('Wagwan!')
-else:
-    print('Not wagwan!') 
+
+# if meaning > 10:
+#     print('Wagwan!')
+# else:
+#     print('Not wagwan!') 
+
+#Ternaly operator
+print('wagwan') if meaning > 10 else print('Not wagwan')
