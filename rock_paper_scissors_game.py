e7f6292827d31289cb55eeca38f45aa4400a60ff
antonomics <antonomics101@gmail.com>
2024-12-11 09:46:32 +0300

    rock paper scissors

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -1,13 +1,21 @@
-import sys
-import random
-from enum import Enum
+
+import sys # Import the sys module to use the sys.exit() function.
+import random # Import the random module to use the random.choice() function.
+from enum import Enum # Import the Enum class from the enum module.
 
 class RPS(Enum):
+    """
+    Enum class representing the possible moves in a Rock-Paper-Scissors game.
+
+    Attributes:
+        ROCK (int): Represents the move "Rock" with a value of 1.
+        PAPER (int): Represents the move "Paper" with a value of 2.
+        SCISSORS (int): Represents the move "Scissors" with a value of 3.
+    """
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
-
-print(RPS(2))
+   
 
 print("")
 playerchoice = input("Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
@@ -19,16 +27,17 @@ player = int(playerchoice)
 if player < 1 or player > 3:
     sys.exit("Invalid choice. Please try again.")
 
-computerchoice = random.choice([1, 2, 3])
+computer = random.randint(1, 3)
+
 
 print("")
-print("You chose " + str(player) + ".")
-print("Python chose " + str(computerchoice) + ".")
+print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+print("Python chose " + str(RPS(computer)).replace('RPS.','') + ".")
 print("")
 
-if (player == 1 and computerchoice == 3) or (player == 2 and computerchoice == 1) or (player == 3 and computerchoice == 2):
+if (player == 1 and computer == 3) or (player == 2 and computer == 1) or (player == 3 and computer == 2):
     print("you win🥳👏")
-elif player == computerchoice:
+elif player == computer:
     print("it's a tie🤝")
 else:
     print("you lose😭")
